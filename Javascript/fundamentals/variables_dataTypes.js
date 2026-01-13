1eaea15ebb73076b00fc58a7944a589536a92386
Shariq Shakeel <shariq.shakeel@apps.local>
2026-01-13 00:13:10 -0800

    first commit

---
+++ Javascript/fundamentals/variables_dataTypes.js
@@ -0,0 +1,41 @@
+// var a = 5;
+// var b = 6;
+
+
+// let c = 5;
+// let d = 7;
+
+
+// const e = "Hello";
+// const f = "World"
+
+
+
+// practice set
+
+//1 create a variable of type string and try to add a number to it
+let a ='shariq';
+console.log(a+1)
+
+
+//2. use typeof operator to find the datatype of variable
+console.log(typeof a)
+
+//3. create a const object in js. cam you change it to hold a number later?
+// answer : yes
+
+const obj ={}
+
+obj.age = 16;
+console.log(obj)
+
+
+// program for a dictionary
+
+const dictionary = {
+    "allwed":"Permitted",
+    "Coarse":"Rough",
+}
+
+console.log(dictionary)
+
